ca305647e226a112f971e152dd8ebdeaf171f544
P <P@P.com>
2018-02-06 15:44:41 +0800

    测试数据结构

--- style/js/init.js
+++ style/js/init.js
@@ -8,17 +8,18 @@ var AddHtml = new Array();　
 AddHtml[0] = "";
 for (var i = 0; i <= groups.length - 1; i++) {
 	var names = JSON.parse(localStorage.getItem('names_'+groups[i]));
-	var urls = JSON.parse(localStorage.getItem('urls_'+groups[i]));
-	if(!names)
-		console.log(names);
-	console.log(urls);
+	var urls = JSON.parse(localStorage.getItem('urls'+groups[i]));
+	if(!names){
+		console.log(names);		
+		console.log(urls);
 		{for (var j = 0; j <= names.length - 1; j++) {
-			AddHtml[i] += "<div class='col-md-3 col-xs-4'><p class='text-center'><a href='" + urls[0] + "' target='_balnk' class='aaa'><img height='60px' width='60px' src='https://www.baidu.com/favicon.ico' alt='...' class='img-circle'><br>	<span class='text1'>" + names[0] +" </span></a></p></div>"
+			AddHtml[i] += "<div class='col-md-3 col-xs-4'><p class='text-center'><a href=' "+ urls[0] + "' target='_balnk' class='aaa'><img height='60px' width='60px' src='https://www.baidu.com/favicon.ico' alt='...' class='img-circle'><br>	<span class='text1'> "+ names[0] +" </span></a></p></div>"
 		}}
-
 	}
 
-	/*初始化界面 开始*/
+}
+
+/*初始化界面 开始*/
 
 
 
